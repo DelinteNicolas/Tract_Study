401d14070e3a409f6400d6d5b4ff635864fe7bb6
Nicolas Delinte <70629561+DelinteNicolas@users.noreply.github.com>
2026-07-07 11:55:35 +0200

    Update skip.sh

--- skip.sh
+++ skip.sh
@@ -6,7 +6,6 @@
 #SBATCH --time=0:01:00
 #SBATCH --mem-per-cpu=100
 #SBATCH --mail-type='FAIL'
-#SBATCH --mail-user='nicolas.delinte@uclouvain.be'
 
 #SBATCH --output='./logs/slurmJob_tracking.out'
 #SBATCH --error='./logs/slurmJob_tracking.err'
